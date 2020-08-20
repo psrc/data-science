05f5a5c6736855558564b2b9bb338bb814d654c3
Suzanne Childress <childresssuzanne@gmail.com>
2020-08-20 15:47:07 -0700

    what data science can do doc, update displacement estimation

--- HHSurvey/displacement_logit_factors.R
+++ HHSurvey/displacement_logit_factors.R
@@ -1,3 +1,4 @@
+
 # You will probably need to install a few libraries to get this to work.
 library(data.table)
 library(tidyverse)
@@ -13,7 +14,7 @@ library(jtools)
 library(ggstance)
 library(sjPlot)
 library(effects)
-
+library(DescTools)
 
 #The codebook is checked in with these code files, named Combined_Codebook_022020.xlsx
 
@@ -23,14 +24,23 @@ library(effects)
 # in poverty, coming from the displacement risk analysis work.
 # it is checked in on github. You will need to point this variable to where it is
 # on your computer.
-displ_index_data<- 'C:/Users/SChildress/Documents/GitHub/data-science/HHSurvey/displacement_risk_estimation.csv'
+displ_index_data<- 'J:/Projects/Surveys/HHTravel/Survey2019/Analysis/displacement/estimation_files/displacement_risk_estimation.csv'
 
 # This commented out file contains very detailed information about parcels.  For example,
 # it contains information for each parcel about the number of jobs within a half mile.
-# It is nearly 1 GB. You may wish to move this file locally for speed reasons.
-parcel_data<- 'C:/Users/SChildress/Documents/HHSurvey/displace_estimate/buffered_parcels.dat'
+# It is nearly 1 GB. You may wish to move this file locally for speed reasons. This is 2018 parcel data
+#parcel_data<- 'C:/Users/SChildress/Documents/HHSurvey/displace_estimate/buffered_parcels.txt'
 # I've moved my locally, as you can see:
-#parcel_data <- 'C:/Users/SChildress/Documents/HHSurvey/displace_estimate/buffered_parcels.dat'
+parcel_data <- 'C:/Users/SChildress/Documents/HHSurvey/displace_estimate/buffered_parcels.dat'
+
+#add updated hh race and age variables
+hh_race_age_cat = read.csv("C:/Users/SChildress/Documents/GitHub/data-science/HHSurvey/hh_race_age_categ.csv")
+
+acs_rent_inc = read.csv("C:/Users/SChildress/Documents/HHSurvey/displace_estimate/acs_income_rent_2018.csv")
+
+dissim<-read.xlsx("C:/Users/SChildress/Documents/HHSurvey/displace_estimate/dissimilarity_2018.xlsx")
+
+
 
 ## Read person-displacement data from Elmer, other travel survey data as well
 db.connect <- function() {
@@ -54,7 +64,7 @@ read.dt <- function(astring, type =c('table_name', 'sqlquery')) {
 }
 
 
-dbtable.person.query<- paste("SELECT *  FROM HHSurvey.v_persons_2017_2019_displace_estimation")
+dbtable.person.query<- paste("SELECT *  FROM HHSurvey.v_persons_2017_2019_displace_estimation_2014parcels")
 person_dt<-read.dt(dbtable.person.query, 'tablename')
 displ_risk_df <- read.csv(displ_index_data)
 parcel_df<-read.table(parcel_data, header=TRUE, sep='')
@@ -64,7 +74,9 @@ parcel_df<-read.table(parcel_data, header=TRUE, sep='')
 #first calculate age and race variables across household members
 # so group people by household
 # calculate if they have any people of color, people over 65
-person_dt[,('hh_all_people_of_color'):= lapply(.SD, function(x) ifelse(all(.SD!='White Only'), 'hh_all_people_of_color', 'hh_not_all_people_of_color')), .SDcols='race_category', by=hhid]
+# Create a person table narrowed to age and race fields
+
+
 person_dt[,('hh_any_older'):= lapply(.SD, function(x) ifelse(any(.SD!='65 years+'), 'hh_any_65p', 'hh_not_all_65p')), .SDcols='age_category', by=hhid]
 person_dt[,('hh_has_children'):= lapply(.SD, function(x) ifelse(any(.SD=='Under 18 years'), 'hh_has_children', 'hh_no_children')), .SDcols='age_category', by=hhid]
 
@@ -105,7 +117,21 @@ for (factor in res_factors){
 #prev_home_taz_2010
 person_df$census_2010_tract <- as.character(person_df$census_2010_tract)
 person_df_dis <- merge(person_df,displ_risk_df, by.x='census_2010_tract', by.y='GEOID', all.x=TRUE)
-# a list of parcel -based variables I'd like to try in the model, there are more on the file
+
+acs_rent_inc$GEOID10<- as.character(acs_rent_inc$GEOID10)
+
+person_df_dis <- merge(person_df_dis,acs_rent_inc, by.x='census_2010_tract', by.y='GEOID10', all.x=TRUE )
+
+
+dissim$GEOID<-as.character(dissim$GEOID)
+
+
+person_df_dis <- merge(person_df_dis,dissim, by.x='census_2010_tract', by.y='GEOID', all.x=TRUE )
+
+
+
+#a list of parcel -based variables I'd like to try in the model, there are more on the file
+
 parcel_based_vars<-c('hh_2', 'stugrd_2', 'stuhgh_2', 'stuuni_2', 'empedu_2', 'empfoo_2', 'empgov_2', 'empind_2',
                       'empmed_2', 'empofc_2', 'empret_2', 'empsvc_2', 'emptot_2', 'ppricdy2', 'pprichr2',
                      'tstops_2', 'nparks_2', 'aparks_2', 'dist_lbus', 'dist_ebus', 'dist_crt', 'dist_fry',
@@ -113,10 +139,19 @@ parcel_based_vars<-c('hh_2', 'stugrd_2', 'stuhgh_2', 'stuuni_2', 'empedu_2', 'em
 
 person_df_dis$census_2010_tract <- as.character(person_df$census_2010_tract)
 
-person_df_dis$parcel_id <- as.character(person_df_dis$parcel_id)
+
+
+person_df_dis$parcel_id <- as.character(person_df_dis$prev_home_parcel_id)
 parcel_df$parcelid <- as.character(parcel_df$parcelid)
-person_df_dis_parcel<- merge(person_df_dis, parcel_df, by.x='parcel_id', by.y='parcelid')
- 
+#merging by the previous residence parcel from the person table and by the parcel id in the parcel table
+person_df_dis_parcel<- merge(person_df_dis, parcel_df, by.x='parcel_id', by.y='parcelid', all.x = TRUE)
+
+
+
+person_df_dis_parcel$hhid <- as.character(person_df_dis_parcel$hhid)
+hh_race_age_cat$household_id <- as.character(hh_race_age_cat$household_id)
+person_df_dis_parcel<- merge(person_df_dis_parcel, hh_race_age_cat, by.x='hhid', by.y='household_id', all.x = TRUE)
+
 # #free up space because the parcel file is huge
 rm(parcel_df)
 # 
@@ -125,23 +160,29 @@ person_df_dis_parcel[parcel_based_vars] <- lapply(person_df_dis_parcel[parcel_ba
 
 
 # There are over a hundred variables on the dataframe- just limit it to potential variables
-vars_to_consider <- c('displaced', 'hh_all_people_of_color', 'hh_any_older', 'hh_has_children',"nonwhite","poor_english","no_bachelors","rent","cost_burdened", 
-                      "severe_cost_burdened","poverty_200"	,
+vars_to_consider <- c('displaced','hh_any_older', 'hh_has_children',"nonwhite","poor_english","no_bachelors","rent","cost_burdened", 
+                      "severe_cost_burdened","poverty_200"	, 'seattle_home',
                       "ln_jobs_auto_30", "ln_jobs_transit_45", "transit_qt_mile","transit_2025_half",
                        "dist_super", "dist_pharm", "dist_rest","dist_park.x",	"dist_school",
-                      "prox_high_inc",	"at_risk_du","voting",
+                      "prox_high_inc",	"at_risk_du","voting",'hhincome_detailed',
                       'displaced', "hhincome_broad", 'race_category', 'education', 'age',
                       'age_category', 'numchildren', 'numadults', 'numworkers','lifecycle','prev_rent_own',
                       'prev_res_type','hhincome_detailed','res_dur', 'hhsize',
-                      'vehicle_count', 'student', 'license','age_category', 'city_name', 'growth_center_name',
+                      'vehicle_count', 'student', 'license','age_category', 'seattle_home', 'city_name', 'growth_center_name',
                       'hh_2', 'stugrd_2', 'stuhgh_2', 'stuuni_2', 'empedu_2', 'empfoo_2', 'empgov_2', 'empind_2',
                       'empmed_2', 'empofc_2', 'empret_2', 'empsvc_2', 'emptot_2', 'ppricdy2', 'pprichr2',
                       'tstops_2', 'nparks_2', 'aparks_2', 'dist_lbus', 'dist_ebus', 'dist_crt', 'dist_fry',
-                      'dist_lrt')
+                      'dist_lrt','hh_race', 'hh_age',
+                      'Percent.Less.than.10K', "Percent.Less.than.25K",  "Percent.Less.than.50K",  
+                      "Percent.50K..100K",    "Percent.Above.100K" ,  "Percent.Above.150K",  "Percent.Above.200K",    
+                      "Median.household.income", "LN.Median.Income","Median.Rent"
+                      )
 
+vars_to_consider<-c(vars_to_consider, names(dissim))
 
-person_df_dis_sm <-person_df_dis_parcel[vars_to_consider]
+#person_df_dis_sm <-person_df_dis_parcel[vars_to_consider]
 
+person_df_dis_sm <-person_df_dis_parcel
 ################# variable aggregations and transformations
 
 
@@ -151,9 +192,12 @@ person_df_dis_sm$college<- with(person_df_dis_sm,ifelse(education %in% c('Bachel
                                                                          'Graduate/post-graduate degree'), 'college', 'no_college'))
 person_df_dis_sm$vehicle_group= 
 with(person_df_dis_sm,ifelse(vehicle_count > numadults, 'careq_gr_adults', 'cars_less_adults')) 
+
 person_df_dis_sm$rent_or_not= 
   with(person_df_dis_sm,ifelse(prev_rent_own == 'Rent', 'Rent', 'Not Rent'))
 
+
+# This data needs to be updated to 2018
 person_df_dis_sm$seattle= 
   with(person_df_dis_sm,ifelse(city_name=='Seattle', 'Seattle', 'Not Seattle')) 
 
@@ -178,56 +222,74 @@ person_df_dis_sm$age_group=
 person_df_dis_sm$moved_lst_yr= 
   with(person_df_dis_sm,ifelse(res_dur=='Less than a year', 'Moved Last Year', 'Moved 1-5 years ago'))
 
-person_df_dis_sm$hhincome_mrbroad <- person_df_dis_sm$hhincome_broad
+person_df_dis_sm$hhincome_mrbroad <- person_df_dis_sm$hhincome_detailed
 #person_df_dis_sm$hhincome_mrbroad[person_df_dis_sm$hhincome_broad== 'Prefer not to answer']<-'100,000-$149,999'
 person_df_dis_sm$hhincome_mrbroad[person_df_dis_sm$hhincome_broad== '$25,000-$49,999']<-'25,000-$99,999'
 person_df_dis_sm$hhincome_mrbroad[person_df_dis_sm$hhincome_broad== '$50,000-$74,999']<-'25,000-$99,999'
 person_df_dis_sm$hhincome_mrbroad[person_df_dis_sm$hhincome_broad== '$75,000-$99,999']<-'25,000-$99,999'
-person_df_dis_sm$hhincome_mrbroad[person_df_dis_sm$hhincome_broad== '$100,000 or more'] <- '$100,000 or more/Prefer not to answer'
-person_df_dis_sm$hhincome_mrbroad[person_df_dis_sm$hhincome_broad== 'Prefer not to answer'] <- '$100,000 or more/Prefer not to answer'
+person_df_dis_sm$hhincome_mrbroad[person_df_dis_sm$hhincome_detailed== '$200,000-$249,999'] <- '$200,000+'
+person_df_dis_sm$hhincome_mrbroad[person_df_dis_sm$hhincome_detailed== '$250,000 or more'] <- '$200,000+'
 
 person_df_dis_sm$dist_prem_bus<-pmin(person_df_dis_sm$dist_ebus, person_df_dis_sm$dist_fry, person_df_dis_sm$dist_lrt)
 person_df_dis_sm$dist_bus<-pmin(person_df_dis_sm$dist_lbus,person_df_dis_sm$dist_ebus, person_df_dis_sm$dist_fry, person_df_dis_sm$dist_lrt)
 
+person_df_dis_sm$hh_age<- as.character(person_df_dis_sm$hh_age)
+person_df_dis_sm$hh_broad_age<-person_df_dis_sm$hh_age
+person_df_dis_sm$hh_broad_age[person_df_dis_sm$hh_age== 'Household with children']<-'HH age 3564 or chil'
+person_df_dis_sm$hh_broad_age[person_df_dis_sm$hh_age== 'Household age 35-64']<-'HH age 3564 or chil'
+
+#new race category based on hh_race
+person_df_dis_sm$hh_race_upd = 
+  with(person_df_dis_sm,ifelse(hh_race == "White", 'White', 'POC/Other'))
+
+person_df_dis_sm$hh_race_asian = 
+  with(person_df_dis_sm,ifelse(hh_race == "Asian", 'Asian', 'non-Asian/White/Other'))
+
+person_df_dis_sm$hh_race_poc = 
+  with(person_df_dis_sm,ifelse(hh_race == "Non-Asian POC", 'Non-Asian POC', 'Asian/White/Other'))
+
+person_df_dis_sm$hh_race_other = 
+  with(person_df_dis_sm,ifelse(hh_race == "Other", 'Other', 'Asian/POC/White'))
+
 person_df_dis_sm[sapply(person_df_dis_sm, is.character)] <- lapply(person_df_dis_sm[sapply(person_df_dis_sm, is.character)], as.factor)
 
-# Variables to Try in the Model Fitting Below
-less_vars<-c('displaced', "hhincome_mrbroad", 'hh_all_people_of_color', 
-            'rent_or_not',
-             'vehicle_group', 'size_group',
-            'seattle',
-            'dist_lrt', 'race_category', 'lifecycle', 'hh_has_children')
- 
-x_sm<-less_vars[!less_vars %in% "displaced"]
-person_df_ls<-person_df_dis_sm[less_vars]
-x_sm<-c(x_sm)
+person_df_dis_sm$Median.household.income = as.numeric(person_df_dis_sm$Median.household.income)
+person_df_dis_sm$Median.Rent= as.numeric(person_df_dis_sm$Median.Rent)
+person_df_dis_sm$sqrt_median_rent = sqrt(1+person_df_dis_sm$Median.Rent)
+person_df_dis_sm$sqrt_median_inc = sqrt(1+person_df_dis_sm$Median.household.income)
 
-# Estimate the model
+#Percent of Trect Population to County Population
+person_df_dis_sm$'White_pct'<- 100*person_df_dis_sm$'White_pct'
+person_df_dis_sm$'Black_pct'<- 100*person_df_dis_sm$'Black_pct'
+person_df_dis_sm$'Asian_pct'<- 100*person_df_dis_sm$'Asian_pct'
+person_df_dis_sm$'Minority_pct'<- 100*person_df_dis_sm$'Minority_pct'
+#iffence in shares between White and other groups					
 
-displ_logit<-glm(displaced ~ hh_all_people_of_color+hhincome_mrbroad+rent_or_not+vehicle_group+seattle+
-                 dist_lrt ,data=person_df_ls,
-                 family = 'binomial')
-summary(displ_logit, correlation= TRUE)
+person_df_dis_sm$'White_Black'<- 100*person_df_dis_sm$'White-Black'
+person_df_dis_sm$'White_Minority'<-100*person_df_dis_sm$'White-Minority'
+person_df_dis_sm$'Black_AIAN'<-100*person_df_dis_sm$'Black-AIAN'
+# displ_logit<-glm(displaced ~. ,data=person_df_ls,
+#                  family = 'binomial')
+
+# displ_logit<-glm(displaced ~ hhincome_detailed*rent_or_not+hh_broad_age*rent_or_not+ 
+#                     hh_race_upd+hh_broad_age+no_bachelors+dist_school+poor_english+sqrt_median_rent+
+#                     hhincome_mrbroad*Percent.Less.than.50K+hhincome_mrbroad*Percent.Above.150K,data=person_df_dis_sm,
+#                   family
+displ_logit<-glm(displaced ~ hhincome_mrbroad+hh_broad_age+rent_or_not
+                 +vehicle_group+Percent.50K..100K+White_pct,
+                   data=person_df_dis_sm,
+                   family = 'binomial')
+
+summary(displ_logit, correlation= TRUE, family = 'binomial')
+
+
+
+
 
 #https://cran.r-project.org/web/packages/jtools/vignettes/summ.html#effect_plot
 
 plot_summs(displ_logit, scale = TRUE)
 
-#https://towardsdatascience.com/visualizing-models-101-using-r-c7c937fc5f04
+PseudoR2(displ_logit, c("McFadden", "Nagel"))
 
-#plot_model(displ_logit, transform = NULL, show.values = TRUE, axis.labels = '', value.offset = .4)
-#effect_plot(plot(allEffects(displ_logit))displ_logit, pred = poor_english, interval = TRUE, plot.points = TRUE)
-#looks nonlinear a bit
 
-#effect_plot(displ_logit, pred = white, interval = TRUE, plot.points = TRUE)
-
-#plot(predictorEffects(displ_logit))
-
-# Trying the bma library, Hana recommended it, but I'm confused about how to use it.
-# x<-person_df_ls[, !names(person_df_ls) %in% c('displaced')]
-# y<-person_df_ls$displaced
-# 
-# glm.out <- bic.glm(x, y, strict = FALSE, OR = 20,
-#                         glm.family="binomial", factor.type=TRUE)
-# summary(glm.out)
-# imageplot.bma(glm.out)
