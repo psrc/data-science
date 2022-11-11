f63e47489c608c0b672b89dbb812577149e022d3
cyyllam <cyyllam@gmail.com>
2022-11-10 16:03:50 -0800

    revised qc check to work with new postcensal data

--- ofm/convert_format_saep.R
+++ ofm/convert_format_saep.R
@@ -48,7 +48,7 @@ read.published.ofm.data <- function() {
   
   hu <- read.xlsx(file.path(pub.dir, "ofm_april1_housing.xlsx"), sheet = "Housing Units", startRow = 4) %>% as.data.table
   hudt <- hu[County %in% counties & Filter %in% filter, ]
-  hucols <- grep("Total\\.Housing\\.Units$", colnames(hudt), value = T)
+  hucols <- grep("Total\\.Housing\\.Units.*$", colnames(hudt), value = T)
   all.hucols <- c("County", hucols)
   h <- hudt[, all.hucols, with = F]
   hmelt <- melt.data.table(h, id.vars = "County", measure.vars = hucols, variable.name = "variable", value.name = "HU")
@@ -73,14 +73,14 @@ qc.rds <- function(years) {
   ofm <- readRDS(file.path(base.dir, dir, "ofm_saep.rds")) %>% as.data.table
   attributes <- c("POP", "HHP","GQ", "HU", "OHU")
   cols <- apply(expand.grid(attributes, years), 1, function(x) paste0(x[1], x[2]))
-  allcols <- c("COUNTYFP10", cols)
+  allcols <- c("COUNTYFP", cols)
   odt <- ofm[, allcols, with = F]
-  dt <- melt.data.table(odt, id.vars = "COUNTYFP10", measure.vars = cols, variable.name = "variable", value.name = "estimate")
+  dt <- melt.data.table(odt, id.vars = "COUNTYFP", measure.vars = cols, variable.name = "variable", value.name = "estimate")
   dt[, `:=` (attribute = str_extract(variable, "[[:alpha:]]+"), YEAR = str_extract(variable, "[[:digit:]]+"))]
-  dtsum <- dt[, lapply(.SD, sum), .SDcols = "estimate", by = .(COUNTY = COUNTYFP10, attribute, YEAR)]
+  dtsum <- dt[, lapply(.SD, sum), .SDcols = "estimate", by = .(COUNTY = COUNTYFP, attribute, YEAR)]
   dtcast <- dcast.data.table(dtsum, COUNTY + YEAR ~ attribute, value.var = "estimate")
   setcolorder(dtcast, c("COUNTY", "YEAR", attributes))
-  d <- dtcast[order(YEAR, COUNTY)][, COUNTYNAME := switch(COUNTY, "033" = "King", "035" = "Kitsap", "053" = "Pierce", "061" = "Snohomish"), by = COUNTY]
+  d <- dtcast[order(YEAR, COUNTY)][, COUNTYNAME := switch(COUNTY, "33" = "King", "35" = "Kitsap", "53" = "Pierce", "61" = "Snohomish"), by = COUNTY]
   
   pdata <- read.published.ofm.data()
   p <- pdata[year %in% years, ]
@@ -91,12 +91,12 @@ qc.rds <- function(years) {
 }
 
 
-convert.file(filename, inputfileformat = "csv", outputfileformat = "rds")
+# convert.file(filename, inputfileformat = "csv", outputfileformat = "rds")
 
 # QC ----------------------------------------------------------------------
 
 
-# years <- c(as.character(2010:2018))
+# years <- c(as.character(2020:2022))
 # dt <- qc.rds(years)
 # print(dt)
 # write.xlsx(dt, file.path(base.dir, dir, "quality_check", paste0("ofm_saep_qc_", Sys.Date(), "_2.xlsx")))
